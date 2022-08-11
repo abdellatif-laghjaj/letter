edba1950a005163890a93cd63babc07eb5e43803
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-11 22:07:16 +0100

    fix some bugs

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const update_now_btn = document.querySelector('.update-now-btn');
 
 //hnaya atbadal link bdak link liba4i ntaya
-const youtube_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+const youtube_url = 'https://www.youtube.com/';
 
-update_now_btn.addEventListener('click', openYouTube(youtube_url));
+update_now_btn.addEventListener('click', function() {
+    openYouTube(youtube_url);
+});
 
 function openYouTube(youtube_url) {
     window.open(youtube_url, '_blank');
